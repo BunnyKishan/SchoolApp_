b63ccdc3eb0874a56a1a28da6d605f92158212b7
Kishan Verma <kishan.ucsy@gmail.com>
2020-09-18 12:04:27 +0630

    adding grid library

--- SchoolProj/App_Start/BundleConfig.cs
+++ SchoolProj/App_Start/BundleConfig.cs
@@ -20,11 +20,15 @@ namespace SchoolProj
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/gridjs.production.min.js"
+                      ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/gridjs.mermaid.min.css"
+                      ));
         }
     }
 }
